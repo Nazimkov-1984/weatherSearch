2d74bcfe0b331d6a1582a5d02a72259c0c42ed58
Mykola <nazimkoff@gmail.com>
2021-10-02 13:34:57 +0300

    add result

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ flagCountry.style.display = "none";
 const randomNumberBackground = randomInteger(1,7);
 document.body.style.backgroundImage = `url("${nameFolder}/${randomNumberBackground}.jpg")`;
 button.addEventListener('click', ()=>{
+
     const input = document.querySelector('.input');
     const city_name = input.value ? input.value: "" ;
     const API_key = '0e3f4c3098c7d2107ce581907ae44eb7';
@@ -18,11 +19,28 @@ button.addEventListener('click', ()=>{
     fetch(url)
     .then(res => res.json())
     .then(data => {
-       
         const codeCountry = data.sys.country;
         flagCountry.style.display = "block";
         flagCountry.src = `https://www.countryflags.io/${codeCountry}/flat/64.png`;
-       
+       console.log(data);
+
+       const dataForLi = [];
+        dataForLi[0]= `Облачность: ${data.clouds.all} %`;
+        dataForLi[1]= `Температура: ${data.main.temp} C${String.fromCharCode(176)}`;
+        dataForLi[2]= `Ощушаеться как: ${data.main.feels_like} C${String.fromCharCode(176)}`;
+        dataForLi[3]= `Влажность: ${data.main.humidity} %`;
+        dataForLi[4]= `Давление: ${data.main.pressure} мм.рт.ст.`;
+        dataForLi[5]= `Максимальная температура: ${data.main.temp_max} C${String.fromCharCode(176)}`;
+        dataForLi[6]= `Минимальная температура: ${data.main.temp_min} C${String.fromCharCode(176)}`;
+        dataForLi[7]= `Направление ветра: ${data.wind.deg} ${String.fromCharCode(176)}`;
+        dataForLi[8]= `Скорость ветра: ${data.wind.speed} м/с.`;
+const itemList = document.querySelectorAll('.result__list__item');
+
+itemList.forEach((item,index) => {
+    item.textContent = dataForLi[index];
+})
+
+
     });
 })
 
